502985c78e27e7fb641c1cf76bc27d30ae192885
himawanmasyaid <himawan.masyaid@gmail.com>
2024-02-05 21:17:35 +0700

    create unit test data type

---
+++ test/tipedate.test.ts
@@ -0,0 +1,16 @@
+
+describe("Data Type", function() {
+
+    it('should must declare', function() {
+
+        let name : string = "Umar Syaid Himawan"
+        let balance : number = 1000000
+        let isVip: boolean = true
+
+        console.info(name)
+        console.info(balance)
+        console.info(isVip)
+
+    });
+
+});
