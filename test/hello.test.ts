e567a1431cc341eab89c08d9e650a65aca69e2b3
himawanmasyaid <himawan.masyaid@gmail.com>
2024-02-05 15:31:52 +0700

    create first hello test with jest

---
+++ test/hello.test.ts
@@ -0,0 +1,12 @@
+
+describe('Hello Word', function () {
+
+    it("Should say hello", function() {
+
+        const name = "Hello Himawan";
+
+        expect(name).toBe("Hello Himawan");
+
+    });
+
+});
